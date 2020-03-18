08349ed200557d9f141030f0a5ca0fb7d4102c83
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-18 18:57:55 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,8 +11,8 @@ def nyc_pigeon_organizer(data)
         if !pigeon_list[name][key1]
           pigeon_list[name][key1] = []
         end
+        binding.pry
       end
-      binding.pry
     end
   end
 end
