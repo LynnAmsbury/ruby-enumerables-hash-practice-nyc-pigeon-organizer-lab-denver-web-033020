4efb123dd2ee47ef375319c1244b0f6173778e02
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-18 19:22:23 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.map do |key1, value1| # .map returns an ARRAY
+    value1.map do |key2, value2|
+      value2.map do |name| # name is a STRING
+        if !pigeon_list[name]
+        pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][key1]
+          pigeon_list[name][key1] = []
+        end
+        pigeon_list[name][key1] << key2.to_s
+      end
+    end
+  end
+  return pigeon_list
 end
