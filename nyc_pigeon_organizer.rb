86e0ed0c95fbfaf8da57f4b55443c92e402d7621
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-18 18:41:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,7 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.map do |key1, value1| # .map returns an ARRAY
-    value.map do |key2, value2|
+    value1.map do |key2, value2|
       binding.pry
     end
 
