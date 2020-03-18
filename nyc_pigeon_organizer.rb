9bf4e5d9890e5c05f80db54dea9764105119c768
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-03-18 18:48:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.map do |key1, value1| # .map returns an ARRAY
     value1.map do |key2, value2|
-      value2.map do |name|
+      value2.map do |name| # name is a STRING
         if !pigeon_list[name]
         pigeon_list[name] = {}
         binding.pry
